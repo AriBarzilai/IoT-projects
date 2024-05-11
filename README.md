133a646d401d3f4f8af82863f5e7323ee08bca59
Ari Barzilai <aribarzilai@gmail.com>
2024-05-11 15:27:37 +0300

    added temperature sensor

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ A series of small mini projects related to Internet of Things, focusing on fun. 
 
 # Projects:
 
-* **Kung Fu Drums:** A combination of several ultrasonic sensors with a buzzer, along with some clever DIY, to create two guantlets which play musical notes when you punch or block.
+* **Kung Fu Drums:** A combination of several ultrasonic sensors with a buzzer, along with some clever DIY, to create two gauntlets which play musical notes when you punch or block.
 
 * **Wizard Hat:** A hat with a temperature sensor and light sensor; turns on LEDs in the darkness and a small electric fan when it is hot
--- hat/hat.ino
+++ hat/hat.ino
@@ -1,10 +1,20 @@
 #include <Adafruit_NeoPixel.h>
+#include "DHT.h"
+
 #define LED_PIN 2
 #define LED_COUNT 12
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define DHTPIN 7
+#define DHTTYPE DHT22
 
-double val;
-int mode = 0;
+#define DEBUG_MODE true
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+DHT dht(DHTPIN, DHTTYPE);
+
+double lightVal;
+float humidity;
+float temperature;
+int gradient_mode = 0;
 
 void setup()
 {
@@ -13,30 +23,39 @@ void setup()
   strip.begin();
   strip.show();
   strip.setBrightness(150);
+  dht.begin();
 }
 
 void loop()
 {
-  val = 1023 - analogRead(A0);
-  Serial.println(val);
+  // update light sensor reading
+  lightVal = 1023 - analogRead(A0);
+  // update humidity + temperature readings (in Celsius)
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
 
-  if (val < 200)
+  logData();
+
+  if (lightVal < 200)
   {
-    switch (mode)
+    switch (gradient_mode)
     {
     case 0:
       colorGradient(12, 0, 12, 50);
+      break;
     case 1:
       colorGradient(0, 12, 12, 50);
+      break;
     case 2:
       colorGradient(12, 0, 12, 50);
+      break;
     }
-    mode = (mode + 1) % 3;
+    gradient_mode = (gradient_mode + 1) % 3;
   }
   else
   {
     colorWipe(strip.Color(0, 0, 0), 50);
-    mode = 0;
+    gradient_mode = 0;
   }
 }
 
@@ -45,7 +64,7 @@ void colorWipe(uint32_t color, int wait)
 {
   for (int i = 0; i < strip.numPixels(); i++)
   {
-    strip.setPixelColor(i, strip.setPixelColor(i, color));
+    strip.setPixelColor(i, color);
     strip.show();
     delay(wait);
   }
@@ -59,5 +78,18 @@ void colorGradient(int R, int G, int B, int wait)
     strip.setPixelColor(i, strip.Color(R * (i + 1), G * (i + 1), B * (i + 1)));
     strip.show();
     delay(wait);
+  }
+}
+
+void logData()
+{
+  if (DEBUG_MODE)
+  {
+    Serial.print("L: ");
+    Serial.print(lightVal);
+    Serial.print("  H: ");
+    Serial.print(humidity);
+    Serial.print("  T: ");
+    Serial.println(temperature);
   }
 }
