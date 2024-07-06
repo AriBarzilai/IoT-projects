0a58e39c0a2dc31e0e11eda7095115f77eb85cf2
Ari Barzilai <aribarzilai@gmail.com>
2024-07-06 22:52:44 +0300

    working buzzer, w.i.p led and webhook

---
+++ FridgeMonitor/src/keys.h
@@ -0,0 +1,7 @@
+#define BLYNK_TEMPLATE_ID "TMPL6u0-uhzo7"
+#define BLYNK_TEMPLATE_NAME "FrostGuard ESP32 Fridge Monitor Project"
+#define BLYNK_AUTH_TOKEN "l15q9VUs_nPujfF6ydEvGM-ha3u6YpL9"
+
+#define WIFI_SSID "crib"
+#define WIFI_PASS "246813579"
+#define WIFI_PASS "135792468"
