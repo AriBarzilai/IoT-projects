39caf4f89116179e1ba12f9785ad3aaa29a3ff96
Avi Lezerovich <96301038+avi-lezerov@users.noreply.github.com>
2024-08-12 14:20:01 +0300

    Add smart watering system implementaiton

---
+++ smartHome/src/plantWateringSystem/plantWateringSystem.cpp
@@ -0,0 +1,67 @@
+#include "plantWateringSystem.h"
+
+plantWateringSystem::plantWateringSystem(int servoPin, int soil_sensor)
+    : servoPin(servoPin), soilSensorPin(soil_sensor) 
+{
+    // initialize();
+}
+
+plantWateringSystem::plantWateringSystem()
+    : servoPin(23), soilSensorPin(34) , moistureThreshold(2000), wateringDuration(5000)
+{
+    // initialize();
+}
+
+void plantWateringSystem::initialize()
+{   
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
+    myservo.setPeriodHertz(50);    // standard 50 hz servo
+    myservo.attach(servoPin, 500, 2400); // attaches the servo onto pin with adjusted pulse width
+    myservo.write(0);                    // sets the servo to 0 degrees (closed)
+    pinMode(soilSensorPin, INPUT);
+}
+
+void plantWateringSystem::startWatering()
+{
+    if (!isWatering)
+    {
+        myservo.write(180); // open the valve (tourn on the pump)
+        isWatering = true;
+        pumpIsOpen = millis();
+    }
+}
+
+void plantWateringSystem::updateMoistureSensor()
+{
+    soilMoistureValue = analogRead(soilSensorPin);
+}
+
+bool plantWateringSystem::needsWatering()
+{
+    return soilMoistureValue > moistureThreshold;
+}
+
+void plantWateringSystem::update()
+{
+    if (isWatering && millis() - pumpIsOpen >= wateringDuration)
+    {
+        myservo.write(0); // close the valve (turn off the pump)
+        isWatering = false;
+    }
+    
+}
+
+void plantWateringSystem::setWateringDuration(int duration)
+{
+    if (duration > 0)
+        wateringDuration = duration;
+}
+
+void plantWateringSystem::setMoistureThreshold(int threshold)
+{
+    if (threshold > 0)
+        moistureThreshold = threshold;
+}
